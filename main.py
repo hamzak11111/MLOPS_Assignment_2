e64a81a671a45355278a44dfe6264b1120ea84da
Hamza Khan <hamza.k11111@gmail.com>
2024-06-13 18:05:27 +0500

    changes committed

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ import pandas as pd
 import os
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
